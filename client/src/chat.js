5ec11ea5de7ff3515064a782c0c978dd2b799a4e
reikhai98 <reikhai@iscity.com.my>
2021-11-13 01:20:13 +0800

    attachement

--- client/src/chat.js
+++ client/src/chat.js
@@ -4,18 +4,137 @@ import SendIcon from "@material-ui/icons/Send";
 import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
 import CloseIcon from "@mui/icons-material/Close";
 import "emoji-mart/css/emoji-mart.css";
-import { motion, AnimateSharedLayout } from "framer-motion";
+import {
+  motion,
+  AnimateSharedLayout,
+  LazyMotion,
+  domAnimation,
+  m,
+} from "framer-motion";
 import data from "emoji-mart/data/google.json";
 import { NimblePicker } from "emoji-mart";
 import AttachFileIcon from "@mui/icons-material/AttachFile";
+import Fab from "@mui/material/Fab";
+import ContactPageRoundedIcon from "@mui/icons-material/ContactPageRounded";
+import AssignmentRoundedIcon from "@mui/icons-material/AssignmentRounded";
+import CameraAltRoundedIcon from "@mui/icons-material/CameraAltRounded";
+import PanoramaRoundedIcon from "@mui/icons-material/PanoramaRounded";
+import Tooltip from "@mui/material/Tooltip";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  customTooltip: {
+    backgroundColor: "white",
+    color: "black",
+    fontSize: "13px",
+  },
+}));
 
 function Chat({ socket, username, receiver, chatData }) {
+  const classes = useStyles();
+
+  const actions = [
+    {
+      icon: (
+        <Tooltip
+          title="Contacts"
+          placement="right"
+          classes={{
+            tooltip: classes.customTooltip,
+            arrow: classes.customArrow,
+          }}
+        >
+          <Fab
+            size="small"
+            sx={{
+              background: "linear-gradient(to right, #00d2ff, #3a7bd5)",
+              color: "white",
+            }}
+          >
+            <ContactPageRoundedIcon />
+          </Fab>
+        </Tooltip>
+      ),
+      name: "Copy",
+    },
+    {
+      icon: (
+        <Tooltip
+          title="Document"
+          placement="right"
+          classes={{
+            tooltip: classes.customTooltip,
+            arrow: classes.customArrow,
+          }}
+        >
+          <Fab
+            size="small"
+            sx={{
+              background: "linear-gradient(to right, #4b6cb7, #182848)",
+              color: "white",
+            }}
+          >
+            <AssignmentRoundedIcon />
+          </Fab>
+        </Tooltip>
+      ),
+      name: "Save",
+    },
+    {
+      icon: (
+        <Tooltip
+          title="Camera"
+          placement="right"
+          classes={{
+            tooltip: classes.customTooltip,
+            arrow: classes.customArrow,
+          }}
+        >
+          <Fab
+            size="small"
+            sx={{
+              background: "linear-gradient(to right, #ff512f, #dd2476)",
+              color: "white",
+            }}
+          >
+            <CameraAltRoundedIcon />
+          </Fab>
+        </Tooltip>
+      ),
+      name: "Print",
+    },
+    {
+      icon: (
+        <Tooltip
+          title="Images & Videos"
+          placement="right"
+          classes={{
+            tooltip: classes.customTooltip,
+            arrow: classes.customArrow,
+          }}
+        >
+          <Fab
+            size="small"
+            sx={{
+              background: "linear-gradient(to right, #da22ff, #9733ee)",
+              color: "white",
+            }}
+          >
+            <PanoramaRoundedIcon />
+          </Fab>
+        </Tooltip>
+      ),
+      name: "Share",
+    },
+  ];
+
   const [currentMessage, setCurrentMessage] = useState("");
   const [messageList, setMessageList] = useState([]);
   const [showPicker, setShowPicker] = useState(false);
   const [showCloseIcon, setCloseIcon] = useState(false);
   const [onlineStatus, setOnlineStatus] = useState("");
   const messagesEndRef = useRef(null);
+  const [showAttach, setShowAttach] = useState(false);
 
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
@@ -120,17 +239,19 @@ function Chat({ socket, username, receiver, chatData }) {
         </div>
 
         {showPicker && (
-          <motion.div className="emoji-container" layout>
-            <NimblePicker
-              include={["recent", "smileys", "people", "foods"]}
-              set="google"
-              data={data}
-              style={{ width: "100%" }}
-              showPreview={false}
-              showSkinTones={false}
-              onClick={onEmojiClick}
-            />
-          </motion.div>
+          <LazyMotion features={domAnimation}>
+            <m.div className="emoji-container" layout>
+              <NimblePicker
+                include={["recent", "smileys", "people", "foods"]}
+                set="google"
+                data={data}
+                style={{ width: "100%" }}
+                showPreview={false}
+                showSkinTones={false}
+                onClick={onEmojiClick}
+              />
+            </m.div>
+          </LazyMotion>
         )}
         <div className="foot">
           <AnimateSharedLayout>
@@ -150,9 +271,32 @@ function Chat({ socket, username, receiver, chatData }) {
               <button onClick={showEmojiPicker}>
                 <InsertEmoticonIcon></InsertEmoticonIcon>
               </button>
-              <button>
-                <AttachFileIcon></AttachFileIcon>
-              </button>
+
+              <div>
+                <button
+                  aria-label="Attach"
+                  onClick={() => setShowAttach(!showAttach)}
+                >
+                  <AttachFileIcon
+                    id="attach"
+                    className={`chat__input-icon ${
+                      showAttach ? "chat__input-icon--pressed" : ""
+                    }`}
+                  ></AttachFileIcon>
+                </button>
+
+                <div
+                  className={`chat__attach ${
+                    showAttach ? "chat__attach--active" : ""
+                  }`}
+                >
+                  {actions.map((btn) => (
+                    <button className="chat__attach-btn" key={btn.name}>
+                      {btn.icon}
+                    </button>
+                  ))}
+                </div>
+              </div>
               <input
                 type="text"
                 className="msg"
